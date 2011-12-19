f78aa533dc7da0ef565bc850fc0c84b153b30aae
Roman Birg <romanbirg@gmail.com>
2011-12-18 18:27:58 -0600

    update apps & props

--- full_maguro.mk
+++ full_maguro.mk
@@ -26,8 +26,11 @@ PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.
 
 # Camera
 PRODUCT_PACKAGES := \
-    Camera
-
+    Camera \
+    Superuser \
+    ROMControl \
+    
+    
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # This is where we'd set a backup provider if we had one
@@ -38,5 +41,13 @@ $(call inherit-product, device/samsung/maguro/device.mk)
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_maguro
 PRODUCT_DEVICE := maguro
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := Full AOSP on Maguro
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_MANUFACTURER := Samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	PRODUCT_NAME=yakju \
+	BUILD_ID=IML74K \
+	BUILD_DISPLAY_ID=IML74K \
+	BUILD_FINGERPRINT="google/yakju/maguro:4.0.2/ICL53F/235179:user/release-keys" \
+	PRIVATE_BUILD_DESC="yakju-user 4.0.2 ICL53F 235179 release-keys"
