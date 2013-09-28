c40fa5f3326d71ead7fbe203e95635410ff2b79f
burstlam <burstlam@gmail.com>
2013-09-29 06:35:13 +0800

    Language

--- cos.mk
+++ cos.mk
@@ -23,6 +23,7 @@ PRODUCT_NAME := cos_maguro
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := samsung
+PRODUCT_LOCALES := zh_CN zh_TW en_US
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.3/JWR66V/737497:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.3 JWR66V 737497 release-keys"
