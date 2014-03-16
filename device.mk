de32992afdb4d49ddd7bcc4e1593577b8433f277
burstlam <burstlam@gmail.com>
2014-03-16 10:21:06 +0800

    Revert "Add a simple chroot jail wrapper for rild"
    This reverts commit f5861bb206fa61828d9bc8dca1927cdc57bec63d.

--- device.mk
+++ device.mk
@@ -30,6 +30,3 @@ PRODUCT_COPY_FILES += \
 
 $(call inherit-product, device/samsung/tuna/device.mk)
 $(call inherit-product-if-exists, vendor/samsung/maguro/maguro-vendor.mk)
-
-PRODUCT_PACKAGES += \
-	rild-sandbox
